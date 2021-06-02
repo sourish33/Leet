ab19bb93d4a5932b6c216413b300e803cf1ed8ab
Sourish Dutta <sourish.d@gmail.com>
2021-06-02 14:36:37 -0500

    twoSum: better

--- TwoSum.js
+++ TwoSum.js
@@ -1,46 +1,65 @@
-let nums = [2,7,11,15]
-let target = 9
 
-let partners = nums.map((el)=>{
-        return target-el
-})
+function twoSum(nums, target){
 
-console.log(partners)
-
-let partnerMap = new Map()
-
-for (let i=0;i<nums.length;i++) {
-    partnerMap.set(nums[i],partners[i])
-}
-
-for (let key of partnerMap.keys()) {
-    let partner = partnerMap.get(key)
-    if (key+partner===target && nums.indexOf(partner)!==-1){
-        console.log(`a: ${nums.indexOf(key)}, b:${nums.indexOf(partner)}`)
-    }
-}
-
-function twoSum(nums, target) {
     let partners = nums.map((el)=>{
         return target-el
     })
 
-    let partnerMap = new Map()
+    let numMap = new Map()
 
-    for (let i=0;i<nums.length;i++) {
-        partnerMap.set(nums[i],partners[i])
+    for (let i=0;i<nums.length;i++){
+        numMap.set(nums[i],i)
     }
 
-    for (let key of partnerMap.keys()) {
-        let partner = partnerMap.get(key)
-        let keyIndex = nums.indexOf(key)
-        let partnerIndex = partners.indexOf(partner)
-        //this leads to a problem if partner and key have the same numerical value, since then indexOf will give the same  value so
-        partnerIndex = partnerIndex === keyIndex ? partnerIndex=nums.indexOf(partner,partnerIndex+1) : partnerIndex
-        if (key+partner===target && partnerIndex!==-1){
-            return [keyIndex, partnerIndex]
+    for (let i=0;i<nums.length;i++){
+        if (numMap.has(partners[i])){
+            console.log([i,numMap.get(partners[i])])
+            if (i!==numMap.get(partners[i])){
+                return([i,numMap.get(partners[i])])
+            }
         }
     }
+
 }
-///////fix the following
-console.log(twoSum([3,5,3,10,3],6))
+
+console.log(twoSum([2,7,11,15],9))
+
+
+
+// let partnerMap = new Map()
+
+// for (let i=0;i<nums.length;i++) {
+//     partnerMap.set(nums[i],partners[i])
+// }
+
+// for (let key of partnerMap.keys()) {
+//     let partner = partnerMap.get(key)
+//     if (key+partner===target && nums.indexOf(partner)!==-1){
+//         console.log(`a: ${nums.indexOf(key)}, b:${nums.indexOf(partner)}`)
+//     }
+// }
+
+// function twoSum(nums, target) {
+//     let partners = nums.map((el)=>{
+//         return target-el
+//     })
+
+//     let partnerMap = new Map()
+
+//     for (let i=0;i<nums.length;i++) {
+//         partnerMap.set(nums[i],partners[i])
+//     }
+
+//     for (let key of partnerMap.keys()) {
+//         let partner = partnerMap.get(key)
+//         let keyIndex = nums.indexOf(key)
+//         let partnerIndex = partners.indexOf(partner)
+//         //this leads to a problem if partner and key have the same numerical value, since then indexOf will give the same  value so
+//         partnerIndex = partnerIndex === keyIndex ? partnerIndex=nums.indexOf(partner,partnerIndex+1) : partnerIndex
+//         if (key+partner===target && partnerIndex!==-1){
+//             return [keyIndex, partnerIndex]
+//         }
+//     }
+// }
+// ///////fix the following
+// console.log(twoSum([3,5,3,10,3],6))
