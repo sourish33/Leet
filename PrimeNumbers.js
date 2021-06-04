66f1906fd02166e09d16b7ed8af48524b1a3d674
Sourish Dutta <sourish.d@gmail.com>
2021-06-04 12:52:30 -0500

    CountPrimes: made into function

--- PrimeNumbers.js
+++ PrimeNumbers.js
@@ -1,29 +1,39 @@
 let n =10
-let spots = Array(n).fill(true)
-spots[0]=false
-spots[1]=false
-spots[2]=false
 
-let primes = [2]
-let lastPrime = 2
+//console.log(primes)
 
-
-while(lastPrime*lastPrime<=n){
-    let i=1
-    while (lastPrime*i<=n){
-        spots[lastPrime*i]=false
-        i++
+function countPrimes(n){
+    if (n<3){
+        return []
     }
-    nextPrime = spots.indexOf(true)
-    primes.push(nextPrime)
-    lastPrime = primes[primes.length-1]
-    spots[nextPrime]=false
-    }
-    // console.log(primes)
-    // console.log(spots)
-let remainders=[...spots.keys()].filter((el)=>spots[el])
-primes = primes.concat(remainders)
-console.log(primes)
+    let spots = Array(n).fill(true)
+    spots[0]=false
+    spots[1]=false
+    spots[2]=false
+
+    let primes = [2]
+    let lastPrime = 2
+
+
+    while(lastPrime*lastPrime<=n){
+        let i=1
+        while (lastPrime*i<=n){
+            spots[lastPrime*i]=false
+            i++
+        }
+        nextPrime = spots.indexOf(true)
+        primes.push(nextPrime)
+        lastPrime = primes[primes.length-1]
+        spots[nextPrime]=false
+        }
+        // console.log(primes)
+        // console.log(spots)
+    let remainders=[...spots.keys()].filter((el)=>spots[el])
+    primes = primes.concat(remainders)
+    return primes
+}
+
+console.log(countPrimes(49))
 
 
 
