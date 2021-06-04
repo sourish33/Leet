154e268fdb57b7e7cb6e09b8075a22af2a234a47
Sourish Dutta <sourish.d@gmail.com>
2021-06-04 11:58:58 -0500

    CountPrimes: single array method started

--- PrimeNumbers.js
+++ PrimeNumbers.js
@@ -1,50 +1,60 @@
+let n =40
+let spots = Array(n+1).fill(1)
+spots[0]=0
+spots[1]=0
+
+let primes = []
+primes.push(spots.indexOf(1))
+let lastPrime = primes[primes.length-1]
 
 
 
-function generateInts(n){
-    let ints=[]
-    for (let count=3;count<n;count++){
-        ints.push(count)
+while(lastPrime*lastPrime<n){
+    let i=1
+    while (lastPrime*i<=n){
+        spots[lastPrime*i]=0
+        i++
     }
-    return ints
+    nextPrime = spots.indexOf(1)
+    primes.push(nextPrime)
+    lastPrime = primes[primes.length-1]
+    spots[nextPrime]=0
 }
-
-function subtractArrays(m, n){
-    //longer array first, shorter array second
-    return m.filter((p)=>!n.includes(p))
-}
+    let remainders=[...spots.keys()].filter((el)=>spots[el]===1)
+    primes = primes.concat(remainders)
+    console.log(primes)
 
 
 
 
-function generateMultiples(base, lim){
-    let mults = []
-    let n =0
-    let mult = 0
-    while(mult<=lim){
-        mult = base*base+n*base
-        mults.push(mult)
-        n++
-    }
-    return mults
-}
+// function generateMultiples(base, lim){
+//     let mults = []
+//     let n =0
+//     let mult = 0
+//     while(mult<=lim){
+//         mult = base*base+n*base
+//         mults.push(mult)
+//         n++
+//     }
+//     return mults
+// }
 
 
-function countPrimes(n){
-    if (n<3){
-        return 0
-    }
-    let primes = []
-    let intArray = generateInts(n)
-    primes.push(2)
+// function countPrimes(n){
+//     if (n<3){
+//         return 0
+//     }
+//     let primes = []
+//     let intArray = generateInts(n)
+//     primes.push(2)
 
-    while (primes[primes.length-1]*primes[primes.length-1]<n){
-        let pMults = generateMultiples(primes[primes.length-1],n)
-        intArray = subtractArrays(intArray, pMults)
-        primes.push(intArray.shift())
-    }
-    primes = primes.concat(intArray)
-    return primes.length
-}
+//     while (primes[primes.length-1]*primes[primes.length-1]<n){
+//         let pMults = generateMultiples(primes[primes.length-1],n)
+//         intArray = subtractArrays(intArray, pMults)
+//         primes.push(intArray.shift())
+//     }
+//     primes = primes.concat(intArray)
+//     return primes.length
+// }
 
-console.log(countPrimes(49979))
+// console.log(countPrimes(49979))
