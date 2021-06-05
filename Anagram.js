4847dc14114e9ad392da9023b8c8ccea6391491b
Sourish Dutta <sourish.d@gmail.com>
2021-06-05 12:30:24 -0500

    Anagrams.js: one hashmap solution

--- Anagram.js
+++ Anagram.js
@@ -4,32 +4,31 @@ function isAnagram(s,t){
         return false
     }
 
-    let anaMap1 = new Map
+    let anaMap = new Map
     for (let char of s){
-        anaMap1.has(char) ? anaMap1.set(char, anaMap1.get(char)+1) : anaMap1.set(char, 1) 
+        anaMap.has(char) ? anaMap.set(char, anaMap.get(char)+1) : anaMap.set(char, 1) 
     }
 
     
-    let anaMap2 = new Map
     for (let char of t){
-        anaMap2.has(char) ? anaMap2.set(char, anaMap2.get(char)+1) : anaMap2.set(char, 1) 
+        if (!anaMap.has(char)) {return false}
+        else{
+            anaMap.set(char, anaMap.get(char) -1 ) 
+        } 
     }
 
-    for (let key of anaMap1.keys()){
-        if (!anaMap2.has(key)){
+    for (let key of anaMap.keys()){
+        if (anaMap.get(key)!==0){
             return false
-        } else{
-            if (anaMap1.get(key)!==anaMap2.get(key)){
-                return false
-            }
         }
-
+ 
     }
     return true
 
 
 }
-console.log(isAnagram("maan","naam"))
+
+console.log(isAnagram("nacthu","nautch"))
 
 
 
